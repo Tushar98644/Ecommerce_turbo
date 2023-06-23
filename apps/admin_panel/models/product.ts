1321bc1c8eee874c0217245732c76c1c1b9c05b0
Turbobot <turbobot@vercel.com>
2023-06-23 19:41:48 +0530

    feat(create-turbo): apply official-starter transform

--- apps/admin_panel/models/product.ts
+++
@@ -1,9 +0,0 @@
-import {Schema} from 'mongoose';
-
-const ProductSchema = new Schema({
-    name: String,
-    price: {type: Number, required: true},
-    description: String,
-});
-
-export default ProductSchema;
--- apps/admin_panel/pages/api/products.ts
+++
@@ -1,4 +0,0 @@
-export default async function handler(req: any, res: any) {
-   res.json(req.body);
-}
-
