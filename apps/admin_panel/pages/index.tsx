670207474f0b99ff3351265f008f9c39ef7adf09
GitHub Action <github-action@users.noreply.github.com>
2023-06-24 04:09:42 +0000

    Format code

--- apps/admin_panel/pages/index.tsx
+++ apps/admin_panel/pages/index.tsx
@@ -3,16 +3,8 @@ import { useSession } from "next-auth/react";
 const Home_page = () => {
   const { data: session } = useSession();
   if (!session) {
-    return <div className="text-5xl text-black">
-        loading...
-      </div> 
-  }
-  else
-    return (
-      <div className="text-5xl">
-        hoem_page is it
-      </div>
-    );
-}
+    return <div className="text-5xl text-black">loading...</div>;
+  } else return <div className="text-5xl">hoem_page is it</div>;
+};
 
 export default Home_page;
