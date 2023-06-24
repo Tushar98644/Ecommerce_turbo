d74b06c355fab89c30109c5676125c032609686f
Tushar98644 <b.toshar@iitg.ac.in>
2023-06-24 09:44:49 +0530

    next auth

--- apps/admin_panel/pages/index.tsx
+++ apps/admin_panel/pages/index.tsx
@@ -1,16 +1,19 @@
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 const Home_page = () => {
   const { data: session } = useSession();
   if (!session) {
-    return <div className="text-5xl text-black">
-        loading...
-      </div> 
+    return (
+      <div className="text-5xl text-black">
+        not logged in
+        <button onClick={()=>signIn('google')}>sign <input type="text" /></button>
+      </div>
+    );
   }
   else
     return (
       <div className="text-5xl">
-        hoem_page is it
+       logged in as {session?.user?.email}
       </div>
     );
 }
