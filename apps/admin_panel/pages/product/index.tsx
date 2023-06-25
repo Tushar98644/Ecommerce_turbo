9871f889550aa22b96438fe5178c385886441652
Tushar98644 <b.toshar@iitg.ac.in>
2023-06-25 20:52:25 +0530

    product table

--- apps/admin_panel/pages/product/index.tsx
+++ apps/admin_panel/pages/product/index.tsx
@@ -1,7 +1,8 @@
 import { Layout } from "@/components";
 import axios from "axios";
+import { get } from "http";
 import Link from "next/link";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Product = () => {
     const [products, setProducts] = useState([]);
@@ -13,17 +14,46 @@ const Product = () => {
 
     return (
         <Layout>
-            <Link href={"/product/new"}>
+            <div className="relative overflow-x-auto w-half lg:w-full md:w-full">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3 text-xl">
+                                Product name
+                            </th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    {products.map((product: any) => (
+                        <tbody>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {product.title}
+                                </th>
+                                <td className="px-6 py-4 flex flex-row gap-8">
+                                    <Link className="btn-default flex flex-row gap-2" href={'/products/edit/' + product._id}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                        </svg>
+                                        <div className="text-white">Edit</div>
+                                    </Link>
+                                    <Link className="btn-default flex flex-row gap-2" href={'/products/edit/' + product._id}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                        </svg>
+                                        <div className="text-white">Delete</div>
+                                    </Link>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    ))}
+                </table>
+            </div>
+            <div className="w-full flex justify-center">
+            <Link href={"/product/new"} className="pt-12 flex items-center">
                 <button className="btn-primary">New product</button>
             </Link>
-            <div className="flex flex-col gap-4">
-                {products.map(product => (
-                    <div className="flex flex-col gap-2 p-4 bg-white rounded-lg shadow-md">
-                        <h1 className="text-xl font-semibold">{product.title}</h1>
-                        <p>{product.description}</p>
-                        <p className="text-xl font-semibold">${product.price}</p>
-                    </div>
-                ))}
             </div>
         </Layout>
     );
