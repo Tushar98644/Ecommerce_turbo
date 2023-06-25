a116d68269b3435582391246eabc610bea9ce3f6
Tushar98644 <b.toshar@iitg.ac.in>
2023-06-25 14:28:37 +0530

    New product page

--- apps/admin_panel/pages/product/new.tsx
+++ apps/admin_panel/pages/product/new.tsx
@@ -1,18 +1,29 @@
 import { Layout } from "@/components";
+import { useState } from "react";
 
 const New_Product = () => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState(0);
+    const createproduct = (e: any) => {
+        e.preventDefault();
+        const data = {title, description, price};
+        console.log(data);
+    };
     return (
         <Layout>
-            <h1 className="mb-12 text-3xl text-blue-900 ">New Product</h1>
-            <h1 className="pb-4 text-blue-900 text-xl">Product Name</h1>
-            <input type="text" placeholder="enter a product name" />
-            <h1 className="text-blue-900 text-xl pb-4">Description</h1>
-            <textarea id="description" placeholder="description"></textarea>
-            <h1  className="text-blue-900 text-xl pb-4">Price(in USD)</h1>
-            <input type="number" placeholder="enter a price" />
-            <div className="items-center flex justify-center">
-                <button className="btn-dark-primary">Save</button>
-            </div>
+            <form action="submit" onSubmit={createproduct} className="w-full">
+                <h1 className="mb-12 text-3xl text-blue-900 ">New Product</h1>
+                <h1 className="pb-4 text-blue-900 text-xl">Product Name</h1>
+                <input type="text" placeholder="enter a product name" value={title} onChange={(e)=>setTitle(e.target.value)}/>
+                <h1 className="text-blue-900 text-xl pb-4">Description</h1>
+                <textarea id="description" placeholder="description"  value={description} onChange={(e)=>setDescription(e.target.value)}></textarea>
+                <h1 className="text-blue-900 text-xl pb-4">Price(in USD)</h1>
+                <input type="number" placeholder="enter a price" value={price} onChange={(e)=>setPrice(e.target.value)}/>
+                <div className="items-center flex justify-center">
+                    <button className="btn-dark-primary" type="submit">Save</button>
+                </div>
+            </form>
         </Layout>
     );
 }
